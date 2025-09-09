8d7d9fcbaa08705385e07e42d5b355a4371cdb79
SKIBIDITOILETFORTNITE <colamola05@gmail.com>
2025-09-09 11:52:37 +0100

    Add files via upload

---
+++ Fortnite External/KM/!!!!!!READMEPLEASE!!!!.txt
@@ -0,0 +1,11 @@
+This driver is not the same as the driver thats used in the cheat!
+This driver is not KM.sys
+This driver is just for test / paste / learn from or anything else!
+If you wanna use this driver you need to do driver swap on the cheat source code!
+
+If you dont know how to driver swap,
+Here is a video on how do driver swap in my youtube channel: https://www.youtube.com/watch?v=Ruax15bRp2Q&t=2s
+
+
+BTW driver.hpp IS THE FILE YOU NEED TO PUT IN YOUR CHEAT FOR THE DRIVER TO COMMUNICATE!
+-SofMain
---
+++ Fortnite External/main.cpp
@@ -0,0 +1,73 @@
+//SIMPLE ASS CODE FOR YALL TO UNDERSTAND EVERYTHING! IF YOU CANT UNDERSTAND YOU A GIGA PASTER
+
+//PLEASE DONT SELL THIS SOURCE PLEASEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+
+//CAUSE I KNOW YOU A PASTER AND DONT KNOW ANYTHING JUST WANNA CHANGE MENU AND NAME AND MAKE THIS YOURS WITHOUT HAVING ANY KNOWLEDGE I AM LETTING YOU KNOW THAT IF YOU CHANGE DRIVER YOU WILL HAVE TO CHANGE SOME THINGS IN HERE AND IN game.hpp and in driver.hpp SO JUST KEEP THAT IN MIND!
+#include "includes.hpp"
+#include <iostream>
+#include <thread>
+
+
+//NO THIS HIGHLIGHTS ARE NOT CHATGPT, I MADE THEM TO HELP YALL YNS TO UNDERSTAND WHAT EACH SHIT DOES, SO THAT YALL CAN LEARN AND NOT JUST PASTE.
+
+
+// DONT USE ANY SHITTY CR3 LOOP FIX OR WHATEVER UR CHEAT WILL LAG SO BAD. IF UR DRIVER NEED CR3 LOOP THEN USE OTHER DRIVER OR FIX IT.
+void main()
+{
+	Start();
+	Sleep(500);
+	printf("Waiting for Fortnite...");
+
+	while (game_wnd == 0)
+	{
+		Sleep(1);
+		sofmain1337::process_id = sofmain1337::find_process(L"FortniteClient-Win64-Shipping.exe"); //waiting for proccess
+		game_wnd = get_process_wnd(sofmain1337::process_id); //found the process
+		Sleep(1);
+	}
+
+	
+
+
+
+	system("cls");
+	printf("SOFMAIN FN BASE V4");
+
+	if (!input::init())
+	{
+		printf("The input was not initialized :("); //input wasnt initialized please go to definition and check //INPUT MEANS THE MOUSE AIM I USE
+		Sleep(3000);
+		exit(0);
+	}
+
+	if (!gui::init())
+	{
+		printf("The gui was not initialized :("); //init/gui wasnt initialized please go to definition and check // GUI MEANS THE OVERLAY I USE
+		Sleep(3000);
+		exit(0);
+	}
+	if (!sofmain1337::find_driver())
+	{
+		printf("The driver was not initialized :("); //driver wasnt initialized please go to definition and check or check ur shitty pc OR YOU JUST FCKED THE COMMS IDK JUST DOUBLE CHECK EVERYTHING FOR THIS ONE!
+		Sleep(3000);
+		exit(0);
+	}
+
+	//DONT TOUCH HERE PASTER! TOUCH ONLY IF YOU WANNA CHANGE DRIVER OR IF YOU KNOW WHAT YOU ARE DOING! -SOFMAIN
+	sofmainisud = sofmain1337::find_image(); //finding img / base
+	auto cr3 = sofmain1337::fetch_cr3(); // CR3 SHIT
+
+	if (!sofmain1337::driver_handle) //base_address //driver_handle
+	{
+		printf("The driver couldn't get the base address"); //Check ur driver base in the code if u get this error! Mine doesnt have a base_address so its driver_handle
+		Sleep(3000);
+		exit(0);
+	}
+	//here is for menu overlay blah blah
+	create_overlay(); 
+	directx_init();
+	render_loop();
+
+}
+
+
